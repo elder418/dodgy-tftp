a35ea64ce1102e52bf34a0a5023d6a1a9a5f83ca
elder420 <elder420@protonmail.com>
2018-09-23 23:12:40 +1000

    doesnt work lol

--- base.cpp
+++ base.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(int argc, char** argv)
 {
+	int sockfd, port;
+	struct sockaddr_in serv_addr;
+	//open ipv4 udp socket
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
+	{
+		std::cout << "failed to open socket\n";
+		return 1;
+	}
+
+	port = 420;
+	serv_addr.sin_family = AF_INET;
+	//insert current host ip into addr struct
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	//convert int to network byte order 
+	serv_addr.sin_port = htons(port);
+
+	if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		std::cout << "failed to bind socket\n";
+		return 1;
+	}
+
+	std::cout << "successfully bound " << serv_addr.sin_addr.s_addr << ":" << serv_addr.sin_port << "\n";
+
+	close(sockfd);
+
 	std::cout << "Jef\n";
 	return 0;
 }
