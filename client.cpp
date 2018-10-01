6641e42936a4b1612ef17fda67338bc3297a2e0a
elder420 <elder420@protonmail.com>
2018-10-01 19:29:25 +1000

    "transfers" file

--- client.cpp
+++ client.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -11,33 +12,89 @@
 int die(const char* err)
 {
 	std::cout << "fatal: " << err << ": " << errno << std::endl;
-	return 1;
+	exit(1);
 }
 
 int main(int argc, char** argv)
 {
 	int err;
+	std::ofstream w_file;
 
 	sock cli_sock("192.168.1.69");
 
-	pack rrq0, recv_pak;
+	pack rrq0, get_pack, put_pack;
 	rrq0.set_opcode(1);
 	rrq0.set_filename("smallertest.txt");
 	rrq0.set_mode("octet");
 
-	std::cout << rrq0.get_opcode() << rrq0.get_filename() << std::endl;
-
 	//send file request
 	cli_sock.set_send_pack(rrq0);
 	if ((err = cli_sock.put()) < 0)
-		return die("put()");
+		die("put()");
 
 	while (1)
 	{
+		//listen
 		if ((err = cli_sock.get()) < 0)
-			return die("get()");
-		recv_pak = cli_sock.get_recv_pack();
-		std::cout << recv_pak.get_opcode() << recv_pak.get_filename() << recv_pak.get_mode() << std::endl;
+			die("get()");
+		std::cout << "packet get!" << std::endl;
+		get_pack = cli_sock.get_recv_pack();
+
+		//identify and respond
+		switch (get_pack.get_opcode())
+		{
+			case 0: //invalid operation
+			{
+				put_pack.set_opcode(5); //error op
+				put_pack.set_errno(4);
+				put_pack.set_errmsg("illegal tftp opcode");
+				cli_sock.set_send_pack(put_pack);
+				break;
+			}
+			case 1: //rrq and wrq invalid on client
+			case 2:
+			{
+				put_pack.set_opcode(5); //error op
+				put_pack.set_errno(4);
+				put_pack.set_errmsg("illegal tftp client opcode");
+				cli_sock.set_send_pack(put_pack);
+				break;
+			}
+			case 3: //data
+			{
+				//open file and write
+				//send ack
+				w_file.open("out-client/smallertest.txt", std::ios::out);
+				if (sizeof(get_pack.get_data()) == 512) //full data, so more to come (probably...)
+				{
+					//DEAL WITH BIG FILE
+					put_pack.set_opcode(6);
+					put_pack.set_errno(666);
+					put_pack.set_errmsg("file too FUCKIN big");
+					cli_sock.set_send_pack(put_pack);
+				}
+				else
+				{
+					//write last bit of data
+					w_file.write(get_pack.get_data(), sizeof(get_pack.get_data()));
+					w_file.close();
+					put_pack.set_opcode(4);
+					put_pack.set_blkno(1);
+				}
+				break;
+			}
+			case 4: //ack
+			{
+				//send next data
+				break;
+			}
+			case 5: //error
+			{
+				//show error and terminate
+				std::cout << get_pack.get_errno() << get_pack.get_errmsg() << std::endl;
+				break;
+			}
+		}
 	}
 	
 	return 0;
--- server.cpp
+++ server.cpp
@@ -12,6 +12,7 @@
 int main(int argc, char** argv)
 {
 	std::ifstream r_file;
+	pack get_pack, put_pack;
 	//open ipv4 udp socket
 	int err;
 
@@ -34,17 +35,85 @@ int main(int argc, char** argv)
 
 		std::cout << "packet get!" << std::endl;
 
-		pack get_pack = srv_sock.get_recv_pack(); //copy socket packet member into memory
+		get_pack = srv_sock.get_recv_pack(); //copy socket packet member into memory
 		//identify packet by opcode and deal w/
 		//for now just echo huehue
 		switch (get_pack.get_opcode())
 		{
 			case 0: //invalid operation
+			{
 				//create error packet with error code 4 = illegal TFTP operation
+				put_pack.set_opcode(5); //error operation
+				put_pack.set_errno(4);
+				put_pack.set_errmsg("illegal tftp opcode");
+				srv_sock.set_send_pack(put_pack);
 				break;
+			}
+			case 1: //RRQ
+			{
+				//read request, check for file, open and put in packet
+				//send data
+				if (strcmp(get_pack.get_filename(), "smallertest.txt") == 0) //see if reqd 'file' exists
+				{
+					std::streampos r_size;
+					char* r_mem;
+					//open file with ptr at end
+					r_file.open("smallertest.txt", std::ios::in | std::ios::ate);
+					r_size = r_file.tellg(); //get file size
+					if (r_size > 512)
+					{
+						put_pack.set_opcode(5); //error operation
+						put_pack.set_errno(666); //boot too big
+						put_pack.set_errmsg("file too big");
+						srv_sock.set_send_pack(put_pack);
+						//big block do split
+					}
+					else
+					{
+						r_mem = new char[r_size];
+						r_file.seekg(0, std::ios::beg);
+						r_file.read(r_mem, r_size);
+						r_file.close();
+						//create data packet
+						put_pack.set_opcode(3);
+						put_pack.set_blkno(1);
+						put_pack.set_data(r_mem);
+						srv_sock.set_send_pack(put_pack);
+					}
+				}
+				else //error packet, file not found.
+				{
+					put_pack.set_opcode(5); //error operation
+					put_pack.set_errno(1); //file not found
+					put_pack.set_errmsg("file not found");
+					srv_sock.set_send_pack(put_pack);
+				}
+				break;
+			}
+			case 2: //WRQ
+			{
+				//write request, open file, read data, put in file
+				//send ack
+				break;
+			}
+			case 3: //DATA
+			{
+				//data block, track numbers, put in file
+				//send ack
+				break;
+			}
+			case 4: //ACK
+			{
+				//ack, block number being acknowledged, 0 for wrq special case
+				//send next data block?
+				break;
+			}
+			case 5: //ERROR
+			{
+				//recv error, cout cleanup and close?
+				break;
+			}
 		}
-		pack put_pack = get_pack;
-		srv_sock.set_send_pack(put_pack);
 		if ((err = srv_sock.put()) < 0)
 		{
 			std::cout << "fatal: put(): " << errno << std::endl;
